ff998cca0333a81c27f9886b34ad10087ad90b59
jennyffermorais <jennyffernovaes@hotmail.com>
2021-04-26 21:33:50 -0300

    adicionar arquivos principais

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 var express = require('express');
 var app = express();
-
-// express vai rodar com base no servidor http nativo do node
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-// evento que é disparado sempre que um cliente se conecta
-// o socket é a instância, a conexão com o cliente
 io.on('connection', (socket) => {
    socket.on('disconnect', () => {
       console.log(` X desconectou: ${socket.id} `);
-   });
-
-   // vai escutar qualquer evento com esse nome que vier desse cliente/socket
-   // recebe os dados que o front enviou
-   // data = dados que o front enviou
-   socket.on('boasvindas', (data) => {
-      console.log(data);
-   });
-
-   socket.on('palavra', (data) => {
-      console.log(data);
-
-      // um evento do back para o front
-      socket.emit('resultado', ` ${data} `);
    });
 });
 
@@ -34,5 +16,5 @@ app.get('/', (req, res) => {
 });
 
 http.listen(4000, () => {
-   console.log('app rodando');
+   console.log('App rodando');
 });
