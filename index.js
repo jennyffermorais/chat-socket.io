8f1b8619361e09192711689c692da17c0554705e
jennyffermorais <jennyffernovaes@hotmail.com>
2021-04-26 21:47:59 -0300

    configurar recebimento de evento no back

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ io.on('connection', (socket) => {
    socket.on('disconnect', () => {
       console.log(` X desconectou: ${socket.id} `);
    });
+
+   // escuta o evento msg e devolve para o front os dados que recebeu
+   socket.on('msg', (data) => {
+      socket.emit('showMsg', data);
+      console.log(data);
+   });
 });
 
 app.set('view engine', 'ejs');
